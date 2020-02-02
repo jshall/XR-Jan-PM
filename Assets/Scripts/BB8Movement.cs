cdcf228f436ce1711ee55d463c229e5cb3023615
Josh Hall <josh.s.hall@gmail.com>
2020-02-02 18:31:53 -0500

    More work on terrain and BB8 control

--- Assets/Scripts/BB8Movement.cs
+++ Assets/Scripts/BB8Movement.cs
@@ -1,35 +1,56 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
-public class BB8Movement : MonoBehaviour
+public class BB8Movement: MonoBehaviour
 {
     public float RotationSpeed = 8;
     public KeyCode Dash = KeyCode.LeftShift;
     public float DashMultiplier = 3;
     public GameObject Head;
+
     private Camera Camera;
+    private Rigidbody Body;
+    private float origAngularDrag;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        Camera = GetComponentInChildren<Camera>();
+
+        Body = GetComponent<Rigidbody>();
+        origAngularDrag = Body.angularDrag;
+        Camera = Head.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame1
     void Update()
     {
-        Head.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+        // Toggle brake
+        if (Input.GetKeyDown(KeyCode.Space))
+            Body.angularDrag = float.IsPositiveInfinity(Body.angularDrag)
+                ? origAngularDrag
+                : float.PositiveInfinity;
+
+        // Toggle light
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+            foreach (var l in GetComponentsInChildren<Light>())
+                l.enabled = !l.enabled;
+
+        // Move head and camera
+        Head.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         if (Camera != null)
         {
-            Camera.transform.Rotate(Vector3.left, Input.GetAxis("Mouse Y"));
+            Camera.transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
             Camera.transform.Translate(0, 0, Input.mouseScrollDelta.y);
         }
+
+        // Move roller
         var input = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
         input *= Time.deltaTime * RotationSpeed * (Input.GetKey(Dash) ? DashMultiplier : 1);
         input = Head.transform.rotation * input;
-        GetComponent<Rigidbody>().AddTorque(input, ForceMode.Impulse);
+        Body.AddTorque(input, ForceMode.Impulse);
     }
 
 }
--- Assets/Scripts/Gyroscope.cs
+++ Assets/Scripts/Gyroscope.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gyroscope : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
