b0cce772a7f578e363ea3a024396bbbb49859610
Josh Hall <josh.s.hall@gmail.com>
2020-02-01 23:35:28 -0500

    better movment

--- Assets/Scripts/BB8Movement.cs
+++ Assets/Scripts/BB8Movement.cs
@@ -3,23 +3,32 @@ using UnityEngine;
 
 public class BB8Movement : MonoBehaviour
 {
-    public float RotationSpeed = 100;
+    public float RotationSpeed = 8;
     public KeyCode Dash = KeyCode.LeftShift;
+    public float DashMultiplier = 3;
     public GameObject Head;
+    private Camera Camera;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Camera = GetComponentInChildren<Camera>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame1
     void Update()
     {
         Head.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
-
-        var input = new Vector3(Input.GetAxis("Vertical"), Head.transform.rotation.y, -Input.GetAxis("Horizontal"));
-        input *= Time.deltaTime * RotationSpeed * (Input.GetKey(Dash) ? 20 : 1);
+        if (Camera != null)
+        {
+            Camera.transform.Rotate(Vector3.left, Input.GetAxis("Mouse Y"));
+            Camera.transform.Translate(0, 0, Input.mouseScrollDelta.y);
+        }
+        var input = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        input *= Time.deltaTime * RotationSpeed * (Input.GetKey(Dash) ? DashMultiplier : 1);
+        input = Head.transform.rotation * input;
         GetComponent<Rigidbody>().AddTorque(input, ForceMode.Impulse);
     }
 
