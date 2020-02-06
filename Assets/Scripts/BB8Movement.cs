0400bf08a4e6eb6c00e44b0cff18adc250a6d3ce
Josh Hall <josh.s.hall@gmail.com>
2020-02-06 01:04:35 -0500

    BB8 tweaks

--- Assets/Scripts/BB8Movement.cs
+++ Assets/Scripts/BB8Movement.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 
-public class BB8Movement: MonoBehaviour
+public class BB8Movement : MonoBehaviour
 {
-    public float RotationSpeed = 8;
-    public KeyCode Dash = KeyCode.LeftShift;
-    public float DashMultiplier = 3;
+    public float RotationSpeed = 20;
+    public float DashMultiplier = 5;
+    public float JumpForce = 20;
     public GameObject Head;
 
     private Camera Camera;
     private Rigidbody Body;
     private float origAngularDrag;
+
+    public KeyCode DashKey = KeyCode.LeftShift;
+    public KeyCode JumpKey = KeyCode.Space;
+    public KeyCode BrakeKey = KeyCode.R;
+    public KeyCode LightKey = KeyCode.F;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +26,21 @@ public class BB8Movement: MonoBehaviour
         Body = GetComponent<Rigidbody>();
         origAngularDrag = Body.angularDrag;
         Camera = Head.GetComponentInChildren<Camera>();
+
+        Body.maxAngularVelocity = float.PositiveInfinity;
     }
 
     // Update is called once per frame1
     void Update()
     {
         // Toggle brake
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(BrakeKey))
             Body.angularDrag = float.IsPositiveInfinity(Body.angularDrag)
                 ? origAngularDrag
                 : float.PositiveInfinity;
 
         // Toggle light
-        if (Input.GetKeyDown(KeyCode.CapsLock))
+        if (Input.GetKeyDown(LightKey))
             foreach (var l in GetComponentsInChildren<Light>())
                 l.enabled = !l.enabled;
 
@@ -48,9 +54,15 @@ public class BB8Movement: MonoBehaviour
 
         // Move roller
         var input = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
-        input *= Time.deltaTime * RotationSpeed * (Input.GetKey(Dash) ? DashMultiplier : 1);
+        input *= RotationSpeed * (Input.GetKey(DashKey) ? DashMultiplier : 1) * Time.deltaTime;
         input = Head.transform.rotation * input;
         Body.AddTorque(input, ForceMode.Impulse);
+        Debug.Log($"input: {input}");
     }
 
+    void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKey(JumpKey) && collision.collider is TerrainCollider)
+                Body.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+    }
 }
