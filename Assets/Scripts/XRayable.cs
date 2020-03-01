c893860b1817e5b4dd8d58f660b84fd235309bb6
Josh Hall <josh.s.hall@gmail.com>
2020-02-29 22:51:01 -0500

    X-Ray

---
+++ Assets/Scripts/XRayable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRayable : MonoBehaviour
+{
+    void Start()
+    {
+        if (GetComponent<Renderer>() is Renderer renderer)
+            renderer.material.renderQueue = 3002;
+    }
+}
